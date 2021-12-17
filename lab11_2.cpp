505bec47a0d6cc9c300ed29d67ebca0d85aef125
gunxroswar <gxz.gunforce@gmail.com>
2021-12-17 17:03:50 +0700

    "เสร็จสิ้นแล็บ10"

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,9 +9,12 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	string text;
+	dest << "-------------------- BOOM ---------------------\n";
+	while(getline(source,text)){
+		dest << text << "\n";
+	}
+	dest << "-------------------- HA!! ---------------------\n";
     source.close();
     dest.close();
 	return 0;
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int num=0;
+    double sum=0,x=0,std,avg;
+    string score;
+    ifstream source("score.txt");
+    while(getline(source,score)){
+        sum += atof(score.c_str());
+        num++;
+        x += pow(atof(score.c_str()),2);
+    }
+    avg = sum/num;
+    std = sqrt((x/num)-pow(avg,2));
+    cout << "Number of data = "<< num << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << avg << "\n";
+    cout << "Standard deviation = " << std;
+}
